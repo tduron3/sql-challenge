51b6011f1956ff2165aa91edeb00aa65cc5c708d
Tony Duron <75818172+tduron3@users.noreply.github.com>
2021-03-11 22:21:50 -0600

    Add files via upload

---
+++ EmployeeSQL/question_1.sql
@@ -0,0 +1,9 @@
+SELECT * FROM employees;
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM salaries
+INNER JOIN employees ON
+employees.emp_no=salaries.emp_no
+
+
+
