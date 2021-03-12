e10a20de29085a2155ad51fe1f149d601c2a74f7
Tony Duron <tonyduron3@att.net>
2021-03-11 19:46:51 -0600

    Add

---
+++ question_3.sql
@@ -0,0 +1,4 @@
+SELECT employees.emp_no, employees.first_name, employees.last_name, departments.dept_no, departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
---
+++ question_5.sql
@@ -0,0 +1,2 @@
+SELECT first_name, last_name, sex FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
