a127b20122aeac81f4ebd9a10fa7db5d8a39acdb
Tony Duron <tonyduron3@att.net>
2021-03-11 23:44:41 -0600

    delete files

--- question_2.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM employees;
-
-SELECT first_name, last_name, hire_date FROM employees
-WHERE hire_date BETWEEN to_date('1986-01-01','YYYY-MM-DD')
-			AND to_date('1987-01-01','YYYY-MM-DD')
-
-
-
-
