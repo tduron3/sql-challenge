83d7c22b13b2efa8363b29518b4f4a79812bb925
Tony Duron <tonyduron3@att.net>
2021-03-11 22:37:25 -0600

    updates

--- question_1.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM employees;
-
-SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
-FROM salaries
-INNER JOIN employees ON
-employees.emp_no=salaries.emp_no
-
-
-
