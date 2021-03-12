d2d648e7c96efc897506f80954d558d894f96f77
Tony Duron <tonyduron3@att.net>
2021-03-11 20:11:23 -0600

    Updates

--- question_3.sql
+++ question_3.sql
@@ -1,4 +1,4 @@
 SELECT employees.emp_no, employees.first_name, employees.last_name, departments.dept_no, departments.dept_name
 FROM employees
-INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
+INNER JOIN dept_manager ON employees.emp_no = dept_manager.emp_no
+INNER JOIN departments ON dept_manager.dept_no = departments.dept_no
