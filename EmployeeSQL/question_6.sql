fd6534d51e0496d16430bbaa3870ebacae4d9b1f
Tony Duron <tonyduron3@att.net>
2021-03-11 22:43:25 -0600

    Update

--- EmployeeSQL/question_6.sql
+++
@@ -1,7 +0,0 @@
-SELECT employees.emp_no, employees.first_name, employees.last_name, departments.dept_name
-FROM employees
-INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
-INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
-AND departments.dept_name = 'Sales'
-
-
